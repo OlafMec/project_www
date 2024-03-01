4775de87791afc4d505b2303382352ec30e65b59
OlafMec <olek.mucho@gmail.com>
2024-03-01 19:11:50 +0100

    add nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const hamburger = document.querySelector('.menu span');
+
+console.log(hamburger);
+
+hamburger.addEventListener('click', () => {
+
+alert('klikniento na menu');
+
+});
+
